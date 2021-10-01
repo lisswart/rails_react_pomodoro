a9af3adc4a5b204a254c5ea714ce671724c2e30a
Lisa Swart <arnoudlisa@gmail.com>
2021-09-30 22:16:54 -0400

    style NavBar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,10 @@
 import '../styles/App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <NavBar />
     </div>
   );
 }
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div id="logo">
+        <h1>Pomodoro Clock</h1>
+      </div>
+      <h3>Hello NavBar</h3>
+    </div>
+  )
+}
+
+export default NavBar
