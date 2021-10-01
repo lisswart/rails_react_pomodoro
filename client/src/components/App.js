8667ca650d2a6cdbe34e46654129fae3be4e666b
Lisa Swart <arnoudlisa@gmail.com>
2021-09-30 23:24:07 -0400

    set up auto-login on frontend with useEffect and useState hooks

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import '../styles/App.css';
 import NavBar from './NavBar';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch('/api/me')
+      .then((r) => {
+        if (r.ok) {
+          r.json().then(userObj => setUser(userObj));
+        }
+      });
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
