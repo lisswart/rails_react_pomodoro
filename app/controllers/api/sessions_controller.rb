8ecd0e7c2eda2dc629b4b2ccfae6055c0488e452
Lisa Swart <arnoudlisa@gmail.com>
2021-10-02 06:56:21 -0400

    needs to debug why user cannot log in after logging out, error: invalid username or password

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
-    if params[:password] == user.password
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: user, status: :created
     else
