1e46c1aaa80e1661ee8748744585cb486ec497a6
Lisa Swart <arnoudlisa@gmail.com>
2021-10-02 08:05:46 -0400

    style timer page

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,12 +1,8 @@
 class Api::SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
-    if params[:password] == user.password
       session[:user_id] = user.id
       render json: user, status: :created
-    else
-      render json: { error: "Invalid username or password" }, status: :unauthorized
-    end
   end
 
   def destroy
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,25 +22,36 @@ function App() {
   return (
     <div className="App">
       <NavBar user={user} setUser={setUser} />
-      <div className="page-on-view">
-        <Switch>
-          <Route path='/categories'>
-            
-          </Route>
-          <Route path='/tasks'>
+        <div className="page-on-view">
+          <Switch>
+            <Route path='/categories'>
+              
+            </Route>
+            <Route path='/tasks'>
 
-          </Route>
-          <Route path='/preferences'>
+            </Route>
+            <Route path='/preferences'>
 
-          </Route>
-          <Route path='/'>
-            <div>
-              <div style={{border: "5px solid cornsilk",height: "35em", width: "35em", borderRadius: "50%", marginTop: "2em"}}></div>
-              <div style={{border: "5px solid cornsilk",height: "35em", width: "35em", borderRadius: "50%", marginTop: "2em"}}></div>
-            </div>
-          </Route>
-        </Switch>
+            </Route>
+            <Route path='/'>
+              <div>
+                <div style={{ display: "flex", flexDirection: "column", margin: 20}}>
+                  <input style={{ margin: 30}} placeholder="task name" />
+                  <input style={{ margin: 30}} placeholder="category" />
+                </div>
+                <div style={{border: "5px solid lemonchiffon",height: "25em", width: "25em", borderRadius: "50%", marginTop: "2em", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                  <div style={{ fontSize: "2em", marginBottom: 20}}>Session</div>
+                  <h1 style={{fontSize: "3em", marginBottom: 30}}>00:00</h1>
+                  <div style={{minWidth: 150, display: "flex", justifyContent: "space-between"}}>
+                    <button style={{width: "fit-content", fontSize: "1.15rem", marginTop: 10, borderRadius: 10, padding: 5, border: "none"}}>Start</button>
+                    <button style={{width: "fit-content", fontSize: "1.15rem", marginTop: 10, borderRadius: 10, padding: 5, border: "none"}}>Reset</button>
+                  </div>
+                </div>
+              </div>
+            </Route>
+          </Switch>
       </div>
+      <footer id="footer">Made with ❤️ by Lisa</footer>
     </div>
   );
 }
