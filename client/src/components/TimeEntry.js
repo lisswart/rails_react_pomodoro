4d1acc25c564038d613db6e682b90e294672296a
Lisa Swart <arnoudlisa@gmail.com>
2021-10-10 17:22:56 -0400

    finish edit UI

--- client/src/components/TimeEntry.js
+++ client/src/components/TimeEntry.js
@@ -4,12 +4,20 @@ function TimeEntry({ timeEntry, deleteTimeEntry, i }) {
     deleteTimeEntry(timeEntry.id);
   }
 
-  function openEditForm(i) {
-    document.querySelectorAll("div.time-entry-task-edit")[i].style.display = "block";
+  function openEditForm(i, str) {
+    if (str === "task") {
+      document.querySelectorAll("div.time-entry-task-edit")[i].style.display = "block";
+    } else if (str === "label") {
+      document.querySelectorAll("div.time-entry-label-edit")[i].style.display = "block";
+    }
   }
 
-  function closeEditForm(i) {
-    document.querySelectorAll("div.time-entry-task-edit")[i].style.display = "none";
+  function closeEditForm(i, str) {
+    if (str === "task") {
+      document.querySelectorAll("div.time-entry-task-edit")[i].style.display = "none";
+    } else if (str === "label") {
+      document.querySelectorAll("div.time-entry-label-edit")[i].style.display = "none";
+    }
   }
 
   function handleSubmit(e) {
@@ -23,7 +31,7 @@ function TimeEntry({ timeEntry, deleteTimeEntry, i }) {
       <td>{ new Date(timeEntry.created_at).toString()}</td>
       <td>
         <button className="edit-button"
-          onClick={() => openEditForm(i)}>
+          onClick={() => openEditForm(i, "task")}>
           {timeEntry.task.task_name}
         </button>
         <div className="form-popup time-entry-task-edit">
@@ -34,16 +42,30 @@ function TimeEntry({ timeEntry, deleteTimeEntry, i }) {
               placeholder="enter taskname" />
             <button type="submit"
               className="save-button"
-              onClick={() => closeEditForm(i)}>
+              onClick={() => closeEditForm(i, "task")}>
                 save
             </button>
           </form>
         </div>
       </td>
       <td>
-        <button className="edit-button">
+        <button className="edit-button"
+          onClick={() => openEditForm(i, "label")}>
           {timeEntry.category.category_label}
         </button>
+        <div className="form-popup time-entry-label-edit">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="label"></label>
+            <input
+              type="text"
+              placeholder="enter label" />
+            <button type="submit"
+              className="save-button"
+              onClick={() => closeEditForm(i, "label")}>
+                save
+            </button>
+          </form>
+        </div>
       </td>             
       <td>{timeEntry.duration}</td>
     </tr>
