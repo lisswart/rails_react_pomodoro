e2c973a0965ea0fafa45e9d31c9b8d96dc15fb0d
Lisa Swart <arnoudlisa@gmail.com>
2021-10-10 11:22:19 -0400

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,9 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
 
-  validates :session_length, numericality: { 
-    only_integer: true,
-    greater_than_or_equal_to: 1,
-    less_than_or_equal_to: 60
-  }, on: :update
+  validates :session_length, numericality: true, on: :update
 
-  validates :break_length, numericality: {
-    greater_than_or_equal_to: 1,
-    less_than_or_equal_to: 60
-  }, on: :update
+  validates :break_length, numericality: true, on: :update
 
   has_secure_password
 end
